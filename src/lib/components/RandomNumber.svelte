ae3fa0f8587c72adeddfc3a0a974c3b75e9347cf
BarbaraHellen <barbarapereira123@hotmail.com>
2025-08-26 22:09:08 -0300

    you might not need to use $effect | untrack

---
+++ src/lib/components/RandomNumber.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import { untrack } from "svelte";
+    let randomnumber = $state(Math.floor(Math.random() * 10));
+    let doubleRandomNumber = $derived.by(() => randomnumber * 2);
+
+    let history: number[] = $state([])
+
+   
+</script>
+
+<h1>Random Number: {randomnumber}</h1>
+<h2>Double Random Number: {doubleRandomNumber}</h2>
+<p>History: {history}</p>
+<button onclick={() => {
+    randomnumber = Math.floor(Math.random() * 10);
+    history = [...history, randomnumber];
+    console.log(randomnumber, doubleRandomNumber);
+}}>generate</button>
