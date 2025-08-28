9f16875a09fc74f31428a8afed37d98c151a50f5
BarbaraHellen <barbarapereira123@hotmail.com>
2025-08-27 22:27:01 -0300

    lucide svelte lib and snippets props children

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
     import Button from "$lib/components/Button.svelte";
-
+    import { Search, Clipboard } from 'lucide-svelte';
     // import Counter from "$lib/components/Counter.svelte";
     // import DisplayName from "$lib/components/DisplayName.svelte";
     // import RandomNumber from "$lib/components/RandomNumber.svelte";
@@ -10,4 +10,13 @@
 <!-- <DisplayName />
 <h1>teste</h1> -->
 <!-- <Counter /> -->
-<Button />
+
+{#snippet buttonLeft()}
+    <Search />
+{/snippet}
+
+{#snippet buttonRight()}
+    <Clipboard />
+{/snippet}
+
+<Button left={buttonLeft} right={buttonRight}>text</Button>
